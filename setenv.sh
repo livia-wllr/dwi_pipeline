faffe8e26e0493bfb478580eb7bf7f0b80c72241
livia-wllr <80101753+livia-wllr@users.noreply.github.com>
2023-03-17 16:32:24 +0100

    Add files via upload

---
+++ setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export FREESURFER_HOME=/Users/4lali/Applications/freesurfer/
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+export SUBJECTS_DIR=/Users/4lali/Documents/WorkWolf/tracula/derivatives-ses-9484/qsiprep/sub-5247/
